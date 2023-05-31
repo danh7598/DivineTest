4b775a5926e29ee3c1769cb53893f99c147297bb
Duy Ta <duy.ta@heatmob.net>
2023-05-31 14:30:26 +0700

    update splash screen

--- App.tsx
+++ App.tsx
@@ -7,13 +7,18 @@
  *
  * @format
  */
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import SplashScreen from 'react-native-splash-screen';
 import {store, persistor} from './src/redux/store';
 import RootNavigation from './src/navigation';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
